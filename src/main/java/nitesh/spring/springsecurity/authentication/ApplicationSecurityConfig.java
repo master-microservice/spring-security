bfd56f583449de1200fa87e686db194d80df924d
Nitesh Nandan <nitesh.na@imimobile.com>
2020-12-06 20:36:05 +0530

    InMemory Authentication

--- src/main/java/nitesh/spring/springsecurity/authentication/ApplicationSecurityConfig.java
+++ src/main/java/nitesh/spring/springsecurity/authentication/ApplicationSecurityConfig.java
@@ -1,24 +1,33 @@
 package nitesh.spring.springsecurity.authentication;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "index","/welcome", "/css/*", "/js/*")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
                 .and()
-                .httpBasic();
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
